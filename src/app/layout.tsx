d1ae32670b434667304221eb173369c734360662
vincentbrowndesign <vincent.brown.design@gmail.com>
2026-04-19 09:17:13 -0500

    fix root layout module

---
+++ src/app/layout.tsx
@@ -0,0 +1,19 @@
+import "./globals.css";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Axis Review",
+  description: "Axis video review",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
